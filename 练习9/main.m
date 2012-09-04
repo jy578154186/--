2ebab3ab62bf4f1ebfa0425b1348a678ff193341
Ibokan <ibokan@heroMan.local>
2012-09-04 09:52:31 +0800

    Initial Commit

---
+++ 练习9/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  练习9
+//
+//  Created by Ibokan on 12-9-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "my_iphoneAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([my_iphoneAppDelegate class]));
+    }
+}
---
+++ 练习9/my_iphoneAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  my_iphoneAppDelegate.h
+//  练习9
+//
+//  Created by Ibokan on 12-9-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface my_iphoneAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
